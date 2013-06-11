499ba689dc9207d7b6b3890b173c5d4f34a40d40
andianderson522 <anaki808built@gmail.com>
2013-06-10 21:20:39 -0400

    some cleanups

--- src/main/java/com/appspot/anaki808built/utils/converters/BooleanConverter.java
+++ src/main/java/com/appspot/anaki808built/utils/converters/BooleanConverter.java
@@ -8,7 +8,7 @@ import java.util.Locale;
 import com.appspot.anaki808built.utils.Assertive;
 
 /**
- * @author andi Jun 10, 2013
+ * @author andi Jun 10, 2013 A null safe converter for booleans
  */
 public final class BooleanConverter {
 
@@ -19,7 +19,7 @@ public final class BooleanConverter {
 	/**
 	 * @param toConvert
 	 *            {@link CharSequence}
-	 * @return true for y, Y, t, T, true, TRUE, yes, YES false for all else
+	 * @return true for y, Y, t, T, true, TRUE, yes, YES, 1. false for all else
 	 */
 	public static boolean convert(final CharSequence toConvert) {
 		if (Assertive.isNull(toConvert)) {
