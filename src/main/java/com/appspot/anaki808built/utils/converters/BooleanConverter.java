44a0b539b84f5ce2b5c9d72a1c5db7d1df491a44
andianderson522 <anaki808built@gmail.com>
2013-06-10 21:20:39 -0400

    some cleanups
    fixing pom issues
    
    adding support for yes, no
    
    adding t, f support
    
    add true false handeling
    
    add numerical c style conversion
    
    refactored method to better name

--- src/main/java/com/appspot/anaki808built/utils/converters/BooleanConverter.java
+++ src/main/java/com/appspot/anaki808built/utils/converters/BooleanConverter.java
@@ -3,12 +3,10 @@
  */
 package com.appspot.anaki808built.utils.converters;
 
-import java.util.Locale;
-
 import com.appspot.anaki808built.utils.Assertive;
 
 /**
- * @author andi Jun 10, 2013
+ * @author andi Jun 10, 2013 A null safe converter for booleans
  */
 public final class BooleanConverter {
 
@@ -19,17 +17,37 @@ public final class BooleanConverter {
 	/**
 	 * @param toConvert
 	 *            {@link CharSequence}
-	 * @return true for y, Y, t, T, true, TRUE, yes, YES false for all else
+	 * @return true for y, Y, t, T, true, TRUE, yes, YES, 1. false for all else
 	 */
-	public static boolean convert(final CharSequence toConvert) {
+	public static boolean convertToBoolean(final CharSequence toConvert) {
 		if (Assertive.isNull(toConvert)) {
 			return false;
 		}
-		final String toCovertUpper = toConvert.toString()
-				.toUpperCase(Locale.US);
-		if ("Y".equals(toCovertUpper)) {
+		final String toConvertString = toConvert.toString();
+		if ("Y".equalsIgnoreCase(toConvertString)) {
+			return true;
+		}
+		if ("YES".equalsIgnoreCase(toConvertString)) {
+			return true;
+		}
+		if ("T".equalsIgnoreCase(toConvertString)) {
+			return true;
+		}
+		if ("TRUE".equalsIgnoreCase(toConvertString)) {
+			return true;
+		}
+		if ("1".equals(toConvertString)) {
 			return true;
 		}
 		return false;
+	}
+
+	/**
+	 * @param toConvert
+	 *            int
+	 * @return true for 1 false for all else
+	 */
+	public static boolean convertToBoolean(final int toConvert) {
+		return convertToBoolean(String.valueOf(toConvert));
 	}
 }
